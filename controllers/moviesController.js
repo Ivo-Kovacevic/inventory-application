da870adf556bf97b3b4db4404bbc23192d4449b3
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-08-26 20:51:23 +0200

    Reorganize css

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -9,11 +9,12 @@ const movieGet = async (req, res) => {
     const movieTitle = decodeURIComponent(req.params.title);
     const movie = await db.getMovieInfo(movieTitle);
     const genres = await db.getMovieGenres(movieTitle);
-    console.log(genres);
     res.render("movie", { movie, genres });
 };
 
 const newMovieGet = async (req, res) => {
+    const query = req.query;
+    console.log(query);
     res.render("newMovie");
 };
 
